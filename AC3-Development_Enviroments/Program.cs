b72f34a898265b1e8e3d2fc0148ef9c5279493c3
Ian Belmonte <a2208013@gmail.com>
2023-01-23 17:49:41 +0100

    Agregar archivos de proyecto.

---
+++ AC3-Development_Enviroments/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ConsoleKeyInfo tecla;
+        do
+        {
+            Console.Clear();
+            ShowTopics();
+            tecla = Console.ReadKey();
+            Console.Clear();
+            switch(tecla.key)
+            {
+                case ConsoleKey.D1:
+                    Topic1();
+                    break;
+                case ConsoleKey.D2:
+                    Topic2();
+                    break;
+                case ConsoleKey.D3:
+                    Topic3();
+                    break;
+                case ConsoleKey.D0:
+                    MsgNextScreen();
+                    break;
+                default:
+                    MsgNextScreen();
+                    break;
+            }
+        } while (tecla.Key != ConsoleKey.D0);
+    }
+}
