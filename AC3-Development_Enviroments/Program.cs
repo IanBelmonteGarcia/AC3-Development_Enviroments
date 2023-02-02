00aac61c08124ec7655a36d6f9a25f8fd0bf8ad5
Ian Belmonte <a2208013@gmail.com>
2023-02-02 15:32:36 +0100

    commit 3

--- AC3-Development_Enviroments/Program.cs
+++ AC3-Development_Enviroments/Program.cs
@@ -46,14 +46,14 @@ internal class Program
         Console.WriteLine("1-Fight Club");
         Console.WriteLine("2-Bullet Train");
         Console.WriteLine("3-Ad Astra");
-        Console.WriteLine("4-The wolf of wall street");
+        Console.WriteLine("4-The wolf of wall street"); //correct
         Console.Write("Reply with a number 1-4: ");
         answ1 = Convert.ToInt32(Console.ReadLine());
         if (answ1==4) count = count + 1;
         Console.Clear();
         Console.WriteLine("In which of these films WAS directed by Tarantino");
         Console.WriteLine("1-Mary Poppins");
-        Console.WriteLine("2-Pulp fiction");
+        Console.WriteLine("2-Pulp fiction"); //correct
         Console.WriteLine("3-Interestellar");
         Console.WriteLine("4-Avatar 2: The Way of The Water");
         Console.Write("Reply with a number 1-4: ");
@@ -64,7 +64,7 @@ internal class Program
         Console.WriteLine("1-Pedro Pascal");
         Console.WriteLine("2-Matt Damon");
         Console.WriteLine("3-Leonardo DiCaprio");
-        Console.WriteLine("4-Antonio Banderas");
+        Console.WriteLine("4-Antonio Banderas"); //correct
         Console.Write("Reply with a number 1-4: ");
         answ3 = Convert.ToInt32(Console.ReadLine());
         if (answ3 == 4) count = count + 1;
@@ -72,7 +72,7 @@ internal class Program
         Console.WriteLine("In which of these films Jake Gyenhall DOESN'T appear");
         Console.WriteLine("1-Nightcrawler");
         Console.WriteLine("2-Donnie Darko");
-        Console.WriteLine("3-American Psycho");
+        Console.WriteLine("3-American Psycho"); //correct
         Console.WriteLine("4-Prisoners");
         Console.Write("Reply with a number 1-4: ");
         answ4 = Convert.ToInt32(Console.ReadLine());
@@ -81,7 +81,7 @@ internal class Program
         Console.WriteLine("Who preformed Batman in the LASTEST Batman film");
         Console.WriteLine("1-Christian Bale");
         Console.WriteLine("2-Ben Affleck");
-        Console.WriteLine("3-Robert Pattinson");
+        Console.WriteLine("3-Robert Pattinson"); //correct
         Console.WriteLine("4-Dwayne 'The Rock' Johnson");
         Console.Write("Reply with a number 1-4: ");
         answ5 = Convert.ToInt32(Console.ReadLine());
@@ -94,7 +94,7 @@ internal class Program
     {
         int answ1, answ2, answ3, answ4, answ5, count = 0;
         Console.WriteLine("What is Pikachu type?");
-        Console.WriteLine("1-Electric");
+        Console.WriteLine("1-Electric"); //correct
         Console.WriteLine("2-Flying");
         Console.WriteLine("3-Electric - Water");
         Console.WriteLine("4-Fire");
@@ -105,14 +105,14 @@ internal class Program
         Console.WriteLine("Which one is NOT a starter pokemon?");
         Console.WriteLine("1-Charmander");
         Console.WriteLine("2-Bulbasaur");
-        Console.WriteLine("3-Staryu");
+        Console.WriteLine("3-Staryu"); //correct
         Console.WriteLine("4-Squirtle");
         Console.Write("Reply with a number 1-4: ");
         answ2 = Convert.ToInt32(Console.ReadLine());
         if (answ2 == 3) count = count + 1;
         Console.Clear();
         Console.WriteLine("What's the name of the protagonist");
-        Console.WriteLine("1-Ash Ketchum");
+        Console.WriteLine("1-Ash Ketchum"); //correct
         Console.WriteLine("2-Brant Peterson");
         Console.WriteLine("3-Peter Parker");
         Console.WriteLine("4-Pikachu");
@@ -121,7 +121,7 @@ internal class Program
         if (answ3 == 1) count = count + 1;
         Console.Clear();
         Console.WriteLine("Which one of this pokemon IS legendary");
-        Console.WriteLine("1-Mewtwo");
+        Console.WriteLine("1-Mewtwo"); //correct
         Console.WriteLine("2-Charizard");
         Console.WriteLine("3-Pikachu");
         Console.WriteLine("4-Quilava");
@@ -131,7 +131,7 @@ internal class Program
         Console.Clear();
         Console.WriteLine("Which one is NOT a pokemon game title");
         Console.WriteLine("1-Pokemon Fire Red");
-        Console.WriteLine("2-Pokemon Water Blue");
+        Console.WriteLine("2-Pokemon Water Blue"); //correct
         Console.WriteLine("3-Pokemon Leaf Green");
         Console.WriteLine("4-Pokemon SoulSilver");
         Console.Write("Reply with a number 1-4: ");
@@ -147,7 +147,7 @@ internal class Program
         Console.WriteLine("Who is the rightful heir to the Iron Throne technically");
         Console.WriteLine("1-Aegon Targaryen");
         Console.WriteLine("2-Daenerys Targaryen");
-        Console.WriteLine("3-Gendry Baratheon");
+        Console.WriteLine("3-Gendry Baratheon"); //correct
         Console.WriteLine("4-Joffrey Baratheon");
         Console.Write("Reply with a number 1-4: ");
         answ1 = Convert.ToInt32(Console.ReadLine());
@@ -156,14 +156,14 @@ internal class Program
         Console.WriteLine("Who was Jon Snow mother?");
         Console.WriteLine("1-Daenerys Targaryen");
         Console.WriteLine("2-Catelyn Stark");
-        Console.WriteLine("3-Lyanna Stark");
+        Console.WriteLine("3-Lyanna Stark"); //correct
         Console.WriteLine("4-Johanna Snow");
         Console.Write("Reply with a number 1-4: ");
         answ2 = Convert.ToInt32(Console.ReadLine());
         if (answ2 == 3) count = count + 1;
         Console.Clear();
         Console.WriteLine("Who was known as: The Spider");
-        Console.WriteLine("1-Lord Varys");
+        Console.WriteLine("1-Lord Varys"); //correct
         Console.WriteLine("2-Olenna Tyrell");
         Console.WriteLine("3-Lord Petyr Baelish");
         Console.WriteLine("4-Corlys Velaryon");
@@ -173,7 +173,7 @@ internal class Program
         Console.Clear();
         Console.WriteLine("What's the name of the BIGGEST dragon known?");
         Console.WriteLine("1-Drogon");
-        Console.WriteLine("2-Belarion");
+        Console.WriteLine("2-Balerion"); //correct
         Console.WriteLine("3-Vhagar");
         Console.WriteLine("4-Caraxes");
         Console.Write("Reply with a number 1-4: ");
@@ -184,7 +184,7 @@ internal class Program
         Console.WriteLine("1-Jorah Mormont");
         Console.WriteLine("2-Eddard 'Ned' Stark");
         Console.WriteLine("3-Robert Baratheon");
-        Console.WriteLine("4-Tywin Lannister");
+        Console.WriteLine("4-Tywin Lannister"); //correct
         Console.Write("Reply with a number 1-4: ");
         answ5 = Convert.ToInt32(Console.ReadLine());
         if (answ5 == 4) count = count + 1;
